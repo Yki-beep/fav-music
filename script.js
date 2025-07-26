0ac10c2a87bd3a8d45d8d51006a9e089ea2651da
Yki-beep <katia0games.ua@gmail.com>
2025-07-27 01:15:27 +0300

    Done (hopefully)

---
+++ script.js
@@ -0,0 +1,59 @@
+
+window.onload = function () {
+    let body = document.querySelector("body");
+    let newSize = localStorage.getItem('fontSize');
+    body.style.fontSize = newSize + "px";
+
+    let theTheme = document.getElementById('theme');
+
+    if (localStorage.getItem("theme") == "light") {
+        theTheme.setAttribute('href', '/styles/light.css');
+    } else {
+        theTheme.setAttribute('href', '/styles/dark.css');
+    }
+}
+// window.onscroll = function() {scrollFunction()};
+
+function toggleTheme() {
+    let theTheme = document.getElementById('theme');
+    if (localStorage.getItem("theme") == "light") {
+        theTheme.setAttribute('href', '/styles/dark.css');
+        localStorage.setItem("theme", "dark");
+    } else {
+        theTheme.setAttribute('href', '/styles/light.css');
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+function fontSizeIncrease() {
+    let body = document.querySelector("body");
+    let fontSize = window.getComputedStyle(body).getPropertyValue('font-size');
+
+    if (parseInt(fontSize.split("px").join("")) < 40) {
+
+        let size = parseInt(fontSize.split("px").join("")) + 2;
+        localStorage.setItem('fontSize', size);
+        body.style.fontSize = size + "px";
+    }
+}
+function fontSizeDecrease() {
+    let body = document.querySelector("body")
+    let fontSize = window.getComputedStyle(body).getPropertyValue('font-size');
+
+    if (parseInt(fontSize.split("px").join("")) > 14) {
+
+        let size = parseInt(fontSize.split("px").join("")) -  2;
+        localStorage.setItem('fontSize', size);
+        body.style.fontSize = size + "px";
+    }
+}
+
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("header").style.fontSize = "30px";
+//   } else {
+//     document.getElementById("header").style.fontSize = "90px";
+//   }
+// }
